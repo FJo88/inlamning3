4dca4ed8f2585d86160ee8c643cdc17e9d04cd7a
Filip Johnsson <filip.j.datatech@gmail.com>
2021-11-23 14:30:59 +0100

    Kommenterat och lagt till bild

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Hämta de olika elementen från HTML:en med diverse selektorer
+// Sparar de sedan i passande variabler
 let header = document.querySelector("h1");
 let questions = document.querySelectorAll("h2");
 let modeButton = document.querySelector("#changeback");
@@ -5,17 +7,21 @@ let resultButton = document.querySelector("#result");
 let label = document.querySelectorAll("#label");
 let inputs = document.querySelectorAll("#choice")
 let checkInput = document.querySelectorAll('input[type="checkbox"]');
-
 let output = document.querySelector("#output")
+
+//Skapar variablerna som behövs
 let darkmode = false;
 let count = 0;
 let answer;
 let check = [];
-let answers = [];
 
-document.body.style.background = "lightblue";
+
+document.body.style.background = "lightgrey";
 modeButton.style.color = "red";
 
+// Kopplar en listener till knappen med id="changeback". Använder en boolean för att kontrollera i vilket mode man 
+// befinner sig i. Loopar sen igenom alla element (input, label, h2) och sätter en lämplig färg på dem. 
+// Ändrar sedan variabeln darkmode
 modeButton.addEventListener("click", () => {
   if (darkmode === false) {
     questions.forEach((element) => {
@@ -37,34 +43,36 @@ modeButton.addEventListener("click", () => {
     });
 
     header.style.color = "black";
-    document.body.style.background = "lightblue";
+    document.body.style.background = "lightgrey";
     darkmode = false;
   }
 });
 
+// I resultButton kopplar man en listener som börjar med att nolla count (Antalet rätt). 
+// Sedan anropar jag mina 3 funktioner.
 resultButton.addEventListener("click", () => {
   count = 0;
   quizAnswer();
-  lastQuestionInput();
+  lastQuestionAnswer();
   createOutput()
 });
 
-
+//Kontrollerar om radiobuttons är ifyllda. Om de är det hämtar man value, kontrollerar sedan om det är true.
+//Value är satt i HTML:en i detta fallet. Räknar upp counter vid rätt svar.
 let quizAnswer = () => {
   inputs.forEach((input) => {
     if(input.checked){
       answer = input.value;
-      console.log(answer);
-      answers.push(answer);
-      if(answer == "true"){
+    if(answer == "true"){
         count++;
       }
     }
   });
  
 }
-
-let lastQuestionInput = () => {
+//Hämtar in checkbox-värdena och sparar in i en array. Kontrollerar sen alla värde i alla element. Om det 
+// överensstämmer med villkoret (rätta svar) så räknas count upp.
+let lastQuestionAnswer = () => {
   check = [];
   checkInput.forEach(item =>{
     let data = item.checked;
@@ -77,7 +85,8 @@ let lastQuestionInput = () => {
 }
 
 
-
+//Skapar utskriften i domen. Förändrar färgen på texten beroende på antalet rätt svar
+//Skapar element och fyller med text samt count-variabeln som håller koll på antal rätt svar. Appendar. 
 let createOutput = () => {
   output.textContent = "";
   let text = document.createElement("h2");
@@ -92,30 +101,13 @@ let createOutput = () => {
   else{
     text.style.color = "orange";
   }
- 
-  text.style.border = "brown solid 10px"
+  
+  text.style.border = "brown solid 10px";
   text.style.padding = "30px";
   output.appendChild(text);
 }
 
 
 
-
-// let getPoints = () =>{
-//   answers.forEach((input) => {
-//     if(input === true){
-//       points++;
-//     }
-//     console.log(points);
-//   })
-// }
-// let q2answer = () => {
-//   input2.forEach((input) => {
-//     if(input.checked){
-//       answer = input.value;
-//       console.log(answer);
-      
-//     }
-//   });
   
 
